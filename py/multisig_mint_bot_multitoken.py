5722e88e641883d32c13f14b34c7b9b40b4d0bae
kayinnnn <138476937+kayinnnn@users.noreply.github.com>
2025-02-20 15:19:12 +0700

    wrap loop in try except

--- py/multisig_mint_bot_multitoken.py
+++ py/multisig_mint_bot_multitoken.py
@@ -54,95 +54,100 @@ mint_block_limit = 10_000_000
 
 if __name__ == "__main__":
     while True:
-        for token in mint_token_map:
-            multisig_mint_token_bal = int(mint_token_map[token]["contract"].functions.balanceOf(multisig).call() /
-                                          10 ** mint_token_map[token]["decimals"])
-            print(mint_token_map[token]["address"], "token bal", multisig_mint_token_bal)
-            if multisig_mint_token_bal > mint_block_limit:
-                multisig_mint_token_bal = mint_block_limit
-            if multisig_mint_token_bal < 1000:
-                time.sleep(10)
-                continue
+        try:
 
-            rfq_url = f"{ethena_url}rfq?pair={token}/USDe&type_={type_}&benefactor={multisig}&side=MINT&size={multisig_mint_token_bal}"
-            response = requests.get(rfq_url, timeout=5)
-            rfq_data = response.json()
+            for token in mint_token_map:
+                multisig_mint_token_bal = int(mint_token_map[token]["contract"].functions.balanceOf(multisig).call() /
+                                              10 ** mint_token_map[token]["decimals"])
+                print(mint_token_map[token]["address"], "token bal", multisig_mint_token_bal)
+                if multisig_mint_token_bal > mint_block_limit:
+                    multisig_mint_token_bal = mint_block_limit
+                if multisig_mint_token_bal < 1000:
+                    time.sleep(10)
+                    continue
 
-            # your own private key. If you trade through smart contract and use delegateSigner, the private key of your delegated signer
-            # pylint: disable=no-value-for-parameter
-            acc = Account.from_key(PRIVATE_KEY)
-            print("rfq_data", rfq_data)
-            print(acc.address)
+                rfq_url = f"{ethena_url}rfq?pair={token}/USDe&type_={type_}&benefactor={multisig}&side=MINT&size={multisig_mint_token_bal}"
+                response = requests.get(rfq_url, timeout=5)
+                rfq_data = response.json()
 
-            # order object sent back to /order endpoint
-            mint_order = {
-                "order_id": str(rfq_data["rfq_id"]),
-                "order_type": "MINT",
-                "expiry": int(time.time() + 60),
-                "nonce": int(time.time() + 60),
-                "benefactor": multisig,
-                "beneficiary": multisig,
-                "collateral_asset": mint_token_map[token]["address"],
-                "collateral_amount": int(rfq_data["collateral_amount"]),
-                "usde_amount": int(rfq_data["usde_amount"]),
-            }
+                # your own private key. If you trade through smart contract and use delegateSigner, the private key of your delegated signer
+                # pylint: disable=no-value-for-parameter
+                acc = Account.from_key(PRIVATE_KEY)
+                print("rfq_data", rfq_data)
+                print(acc.address)
 
-            # EIP712 signature struct
-            # "Order(
-            # string order_id,
-            # uint8 order_type,
-            # uint256 expiry,
-            # uint256 nonce,
-            # address benefactor,
-            # address beneficiary,
-            # address collateral_asset,
-            # uint256 collateral_amount,
-            # uint256 usde_amount)"
+                # order object sent back to /order endpoint
+                mint_order = {
+                    "order_id": str(rfq_data["rfq_id"]),
+                    "order_type": "MINT",
+                    "expiry": int(time.time() + 60),
+                    "nonce": int(time.time() + 60),
+                    "benefactor": multisig,
+                    "beneficiary": multisig,
+                    "collateral_asset": mint_token_map[token]["address"],
+                    "collateral_amount": int(rfq_data["collateral_amount"]),
+                    "usde_amount": int(rfq_data["usde_amount"]),
+                }
 
-            # Tuple for signing
-            order_tuple = (
-                str(mint_order["order_id"]),  # string order_id, use RFQ ID from /rfq payload
-                (
-                    0 if mint_order["order_type"].upper() == "MINT" else 1
-                ),  # uint8 order_type, 0 for mint, 1 for redeem
-                mint_order["expiry"],  # uint256 expiry, use current time + 60s
-                mint_order[
-                    "nonce"
-                ],  # uint256 nonce, use unique int for each order (eg timestamp)
-                w3.to_checksum_address(mint_order["benefactor"]),
-                # address benefactor, address where token is taken out of. stETH for mint, USDe for redeem
-                w3.to_checksum_address(mint_order["beneficiary"]),
-                # address beneficiary, address where incoming token is received. USDe for mint, stETH for redeem
-                w3.to_checksum_address(
-                    mint_order["collateral_asset"]
-                ),  # address collateral_asset, stETH address
-                mint_order[
-                    "collateral_amount"
-                ],  # uint256 collateral_amount, use exact value provided in RFQ
-                mint_order[
-                    "usde_amount"
-                ],  # uint256 usde_amount, use exact value provided in RFQ
-            )
+                # EIP712 signature struct
+                # "Order(
+                # string order_id,
+                # uint8 order_type,
+                # uint256 expiry,
+                # uint256 nonce,
+                # address benefactor,
+                # address beneficiary,
+                # address collateral_asset,
+                # uint256 collateral_amount,
+                # uint256 usde_amount)"
 
-            order_hash = defi_minting_contract.functions.hashOrder(order_tuple).call()
-            # Convert the hash to SignableMessage format
-            # pylint: disable=no-member
-            order_signed = acc.unsafe_sign_hash(order_hash)
-            order_rsv = (
-                to_bytes(order_signed.r) + to_bytes(order_signed.s) + to_bytes(order_signed.v)
-            )
-            print("tuple", order_tuple)
-            print("rsv", order_rsv)
-            signature = Signature(SignatureType.EIP712, order_rsv)
+                # Tuple for signing
+                order_tuple = (
+                    str(mint_order["order_id"]),  # string order_id, use RFQ ID from /rfq payload
+                    (
+                        0 if mint_order["order_type"].upper() == "MINT" else 1
+                    ),  # uint8 order_type, 0 for mint, 1 for redeem
+                    mint_order["expiry"],  # uint256 expiry, use current time + 60s
+                    mint_order[
+                        "nonce"
+                    ],  # uint256 nonce, use unique int for each order (eg timestamp)
+                    w3.to_checksum_address(mint_order["benefactor"]),
+                    # address benefactor, address where token is taken out of. stETH for mint, USDe for redeem
+                    w3.to_checksum_address(mint_order["beneficiary"]),
+                    # address beneficiary, address where incoming token is received. USDe for mint, stETH for redeem
+                    w3.to_checksum_address(
+                        mint_order["collateral_asset"]
+                    ),  # address collateral_asset, stETH address
+                    mint_order[
+                        "collateral_amount"
+                    ],  # uint256 collateral_amount, use exact value provided in RFQ
+                    mint_order[
+                        "usde_amount"
+                    ],  # uint256 usde_amount, use exact value provided in RFQ
+                )
 
-            signature_hex = to_hex(signature.signature_bytes)
-            print("hex", signature_hex)
+                order_hash = defi_minting_contract.functions.hashOrder(order_tuple).call()
+                # Convert the hash to SignableMessage format
+                # pylint: disable=no-member
+                order_signed = acc.unsafe_sign_hash(order_hash)
+                order_rsv = (
+                    to_bytes(order_signed.r) + to_bytes(order_signed.s) + to_bytes(order_signed.v)
+                )
+                print("tuple", order_tuple)
+                print("rsv", order_rsv)
+                signature = Signature(SignatureType.EIP712, order_rsv)
 
-            url = f"""{ethena_url}order?signature={signature_hex}"""
-            response = requests.post(url, json=mint_order, timeout=60)
-            if "error" in response.json():
-                print(response.json()["error"])
-            else:
-                tx_id = response.json()["tx"]
-                print(f"https://etherscan.io/tx/{tx_id}")
-            time.sleep(15)
+                signature_hex = to_hex(signature.signature_bytes)
+                print("hex", signature_hex)
+
+                url = f"""{ethena_url}order?signature={signature_hex}"""
+                response = requests.post(url, json=mint_order, timeout=60)
+                if "error" in response.json():
+                    print(response.json()["error"])
+                else:
+                    tx_id = response.json()["tx"]
+                    print(f"https://etherscan.io/tx/{tx_id}")
+                time.sleep(15)
+
+        except Exception as e:
+            print(e)
